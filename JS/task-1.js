d4f690a39ba5b87765bbbe23c8a738ccd3811f09
Natalya7002 <140949227+Natalya7002@users.noreply.github.com>
2023-10-27 18:21:51 +0300

    variant1

---
+++ JS/task-1.js
@@ -0,0 +1,17 @@
+function slugify(title) {
+  return title.toLowerCase().replace(/\s+/g, "-");
+}
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+// function slugify(title) {
+//   let titleLowerCase = title.toLowerCase();
+//   let titleReplace = titleLowerCase.replace(/ /g, "-");
+//   return titleReplace;
+// }
+// console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+// console.log(slugify("English for developer")); // "english-for-developer"
+// console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
